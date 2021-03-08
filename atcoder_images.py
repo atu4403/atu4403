ef3fc2f38addcb9ddb52c88742538838ad1bc533
atu4403 <73111778+atu4403@users.noreply.github.com>
2021-03-08 23:10:09 +0900

    create

---
+++ atcoder_images.py
@@ -0,0 +1,23 @@
+import os
+import pathlib
+from selenium import webdriver
+
+USERNAME = os.environ.get("ATCODER_USERNAME")
+assert USERNAME, "Is required env.ATCODER_USERNAME"
+PATH1 = os.environ.get("ATCODER_STATUS_PATH") or "images/ratingStatus.png"
+PATH2 = os.environ.get("ATCODER_GRAPH_PATH") or "images/ratingGraph.png"
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+driver = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub",
+    desired_capabilities=options.to_capabilities(),
+    options=options,
+)
+driver.get(f"https://atcoder.jp/users/{USERNAME}")
+driver.implicitly_wait(10)
+png1 = driver.find_element_by_id("ratingStatus").screenshot_as_png
+png2 = driver.find_element_by_id("ratingGraph").screenshot_as_png
+pathlib.Path(PATH1).write_bytes(png1)
+pathlib.Path(PATH2).write_bytes(png2)
+driver.quit()
