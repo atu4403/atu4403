4a0364613089a4ea6d7e2781b6c0cf3d2e9b6b79
atu4403 <73111778+atu4403@users.noreply.github.com>
2021-03-21 14:29:24 +0900

    set timezone

--- update.py
+++ update.py
@@ -1,9 +1,10 @@
 import re
 import pathlib
-import datetime as dt
+from datetime import datetime, timedelta, timezone
 
+JST = timezone(timedelta(hours=+9), "JST")
 reg = re.compile(r"last update:.*")
 p = pathlib.Path("README.md")
 s = p.read_text()
-t = re.sub(reg, f"last update: {dt.datetime.now().strftime('%Y-%m-%d %H:%M:%S')})", s)
+t = re.sub(reg, f"last update: {datetime.now(JST).strftime('%Y-%m-%d %H:%M:%S')})", s)
 p.write_text(t)
